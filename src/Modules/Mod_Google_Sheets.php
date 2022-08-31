c4c39755a53da2b09c329a14298917841e2ac94d
Luis Gonzalez <lgonzal@redhat.com>
2022-08-31 18:24:18 -0400

    minor updates

--- src/Modules/Mod_Google_Sheets.php
+++ src/Modules/Mod_Google_Sheets.php
@@ -8,18 +8,18 @@ namespace Main\Modules;
             $this->client->setApplicationName('Google Sheets and PHP');
             $this->client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
             $this->client->setAccessType('offline');
-            $this->client->setAuthConfig(__DIR__ . '/credentials.json');
+            $this->client->setAuthConfig(__DIR__ . '/google_credentials.json');
 
             $this->service = new Google_Service_Sheets($client);
 
             $this->spreadsheetId = $spreadsheetId;
+            $this->arrOne = ['one'];
+            $this->arrTwo = ['two'];
+            $this->arrValues = [$this->arrOne, $this->arrTwo]];
         }
 
-        public function get_sheet_rows($get_range =  "A2: B10") {
-            $response = $this->service->spreadsheets_values->get($this->spreadsheetId, $get_range);
-            return $arrValues; 
-        }
-        public function update_sheet_rows($update_range =  "A2: B10", $arrValues = [[$arrOne, $arrTwo]]) {
+
+        public function update_sheet_rows($update_range =  "A2: B10", $arrValues = $this->arrValues) {
 
             $body = new Google_Service_Sheets_ValueRange([
 
@@ -33,6 +33,12 @@ namespace Main\Modules;
           
               ];
             $update_sheet = $this->service->spreadsheets_values->update($this->spreadsheetId, $update_range, $body, $params);
+           // return $update_sheet;
+        }
+
+        public function get_sheet_rows($get_range =  "A2: B10") {
+            $response = $this->service->spreadsheets_values->get($this->spreadsheetId, $get_range);
+            return $arrValues; 
         }
 
     }
