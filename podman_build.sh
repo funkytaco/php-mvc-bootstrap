44ef7b56a911d324174cfd13b618f7c1c91a1937
Luis Gonzalez <weehoo@gmail.com>
2024-11-29 07:37:17 -0500

    Update mount path. This allows you to bash into container and run "composer unlock && composer install-mvc" to edit files locally and have them update in the container

--- podman_build.sh
+++ podman_build.sh
@@ -2,4 +2,4 @@
 echo Building podman image.
 podman build -t localhost/php-mvc-apache .
 echo Running podman container...
-podman run -v `pwd`:/opt -p 80:80 localhost/php-mvc-apache
+podman run -v `pwd`:/var/www -p 80:80 localhost/php-mvc-apache
