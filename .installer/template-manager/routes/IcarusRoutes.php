c87bb2c512dc8bdc1d3027c5c1d389031709ee51
Luis Gonzalez <weehoo@gmail.com>
2024-12-20 17:08:45 -0500

    Fixed loading templates

--- .installer/template-manager/routes/IcarusRoutes.php
+++ .installer/template-manager/routes/IcarusRoutes.php
@@ -15,7 +15,7 @@
         ['POST', '/template-manager/preview', function($request, $response) use ($container) {
             return $container->get('Icarus\Controllers\TemplateManagerController')->previewTemplate($request, $response);
         }],
-        ['GET', '/api/templates/[i:id]', function ($request, $response) use ($container) {
+        ['GET', '/api/templates/id/[*:id]', function ($request, $response) use ($container) {
             return $container->get('Icarus\Controllers\TemplateManagerController')->getTemplate($request, $response);
         }],
         ['POST', '/api/templates/[i:id]', function ($request, $response) use ($container) {
